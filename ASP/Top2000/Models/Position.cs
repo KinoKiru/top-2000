b2d265f7e668cda52e50f940dc6ff9dd15ff471b
KinoKiru <youriboulanger@gmail.com>
2023-01-12 14:06:24 +0100

    added Position Model

--- ASP/Top2000/Models/Position.cs
+++ ASP/Top2000/Models/Position.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-    internal class Position
+    public class Position
     {
+
+        public DateOnly Year { get; set; }
+
+        public int Place { get; set; }
+
+        [ForeignKey("SongId")]
+        public int SongID { get; set; }
+        public Song Song { get; set; } = new Song();
     }
 }
