50966f5e59d71738133d07545f8331813a2b2935
KinoKiru <youriboulanger@gmail.com>
2023-01-12 13:55:33 +0100

    bug fix

--- ASP/Top2000/Models/Song.cs
+++ ASP/Top2000/Models/Song.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
@@ -8,8 +9,10 @@ namespace Models
 
         [StringLength(75)]
         public string Title { get; set; } = string.Empty;
+
+        [ForeignKey("ArtistId")]
         public int ArtistId { get; set; }
-        public Artist Artist { get; set; }
+        public Artist Artist { get; set; } = new Artist();
         public byte[]? Photo { get; set; }
     }
 }
