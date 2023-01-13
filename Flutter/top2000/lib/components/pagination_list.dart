a03b853c4774a21e6f61857d051b99e329465999
KinoKiru <youriboulanger@gmail.com>
2023-01-13 12:43:02 +0100

    added owo

---
+++ Flutter/top2000/lib/components/pagination_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+/// TranslationWrapper
+/// Shorthand code for StreamBuilder widget
+class PaginationList<T> extends StatefulWidget {
+  const PaginationList(
+      {super.key,
+      required this.onEnd,
+      required this.items,
+      required this.itemBuilder});
+  final Function onEnd;
+  final List<T> items;
+  final Widget Function(BuildContext context, int index) itemBuilder;
+
+  @override
+  State<PaginationList<T>> createState() => _PaginationListState<T>();
+}
+
+class _PaginationListState<T> extends State<PaginationList<T>> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: widget.items.length,
+      itemBuilder: widget.itemBuilder,
+    );
+  }
+}
