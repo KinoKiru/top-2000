b44348b46ec151cf015a28f25f8454ff9eb945cd
Youri Blanckenborg <0331038@student.rocvantwente.nl>
2023-01-16 09:57:51 +0100

    Revert "Revert "added owo""
    This reverts commit a077a290d776b129eaed9b943cab399f95095599.

--- Flutter/top2000/lib/pages/home.dart
+++ Flutter/top2000/lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:top2000/components/pagination_list.dart';
 import 'package:top2000/components/translation_wrapper.dart';
+import 'package:top2000/generators/songs.dart';
+import 'package:top2000/models/song.dart';
 import 'package:top2000/utils/utils.dart';
 
 class Home extends StatefulWidget {
@@ -10,26 +13,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Song> songs = getSongs(length: 10);
+
   @override
   Widget build(BuildContext context) {
     final double sizeX = MediaQuery.of(context).size.width;
     final double sizeY = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
           toolbarHeight: sizeX < sizeY ? 100 : 60,
           title: responsiveHomeAppBar(sizeX, sizeY, context)),
       body: TranslationWrapper(
-        onLoad: (BuildContext context) => Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'UwU',
-              ),
-            ],
-          ),
-        ),
-      ),
+          onLoad: (BuildContext context) => Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: PaginationList<Song>(
+                  items: getSongs(length: 100),
+                  itemBuilder: (BuildContext context, int index) =>
+                      Text(index.toString()),
+                  onEnd: () {},
+                ),
+              )),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
