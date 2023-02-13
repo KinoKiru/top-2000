d2572187d3b0c87a58db3f085bebceee366d9c57
KinoKiru <youriboulanger@gmail.com>
2023-02-13 15:43:32 +0100

    added partial controllers

---
+++ ASP/Top2000/Top2000.Web/Controllers/RoutingController.0328405.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public partial class RoutingController : Controller
+    {
+       
+    }
+}
---
+++ ASP/Top2000/Top2000.Web/Controllers/RoutingController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public partial class RoutingController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RoutingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
